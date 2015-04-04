f2a29d9e8d5bbc0707c0ee6b612524a482ad17ef
Johannes Eichler <jo.eichler@gmx.de>
2015-04-05 00:44:54 +0200

    orientation trouble fixed

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -17,14 +17,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         // The status bar should be hidden while in meme edit view to use the maximum available screen size for the image.
-        
-        self.hideStatusBar()
+
     }
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(true)
 
-        self.subscribeToOrientationChangeNotification()
     
     }
 
@@ -33,33 +31,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func subscribeToOrientationChangeNotification() {
-        
-        // To check if the statusBar is visible afte orientation change
-        
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "hideStatusBar", name: UIDeviceOrientationDidChangeNotification, object: nil)
-    
-    }
-    
-    func hideStatusBar(){
-        // Checks if the status bar is visible and adjusts the memeEditorImage height accordingly. 
-        
-        if (!UIApplication.sharedApplication().statusBarHidden){
-            
-            UIApplication.sharedApplication().setStatusBarHidden(true, withAnimation: UIStatusBarAnimation.Slide)
-
-        }
-        else {
-            
-            println("The Status Bar is visible")
-        
-        
-        }
-        
-    
-    
-    }
-
 
 
     @IBAction func photoFromCamera(sender: UIBarButtonItem) {
