ecb986a02ed5b3cd4d8fc445c601cb19580f6727
Johannes Eichler <jo.eichler@gmx.de>
2015-04-04 22:17:43 +0200

    Basic Layout Meme Editor

---
+++ Meme.swift
@@ -0,0 +1,28 @@
+//
+//  Meme.swift
+//  imagePickerTest
+//
+//  Created by Johannes Eichler on 01.04.15.
+//  Copyright (c) 2015 Eichler. All rights reserved.
+//
+
+import UIKit
+
+class Meme: NSObject {
+    
+    var topText: String!
+    var bottomText: String!
+    var originalImage: UIImage!
+    var memedImage: UIImage!
+    
+    init(text:String, bottomText:String, originalImage:UIImage, memedImage:UIImage) {
+
+        super.init()
+        self.topText = text
+        self.bottomText = bottomText
+        self.originalImage = originalImage
+        self.memedImage = memedImage
+        
+    }
+   
+}
---
+++ MemedImageContext.swift
@@ -0,0 +1,40 @@
+//
+//  MemedImageContext.swift
+//  imagePickerTest
+//
+//  Created by Johannes Eichler on 01.04.15.
+//  Copyright (c) 2015 Eichler. All rights reserved.
+//
+/* This class gets the current context of a memed image and provides functionality to render it
+*/
+
+import UIKit
+
+class memedImageContext: NSObject {
+    
+    var currentView: UIImageView!
+    
+    
+    init(currentView: UIImageView) {
+        
+        super.init()
+        self.currentView = currentView
+    }
+    
+    func generateMemedImage() -> UIImage
+    {
+        //Generate a memed image fromt the context of self.currentView
+        
+        UIGraphicsBeginImageContext(self.currentView.superview!.frame.size)
+        
+        self.currentView.drawViewHierarchyInRect(self.currentView.frame,
+            afterScreenUpdates: true)
+        let memedImage : UIImage =
+        UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()        
+
+        
+        return memedImage
+    }
+   
+}
