ea80b02625032e83238411879b23776e2e49cce6
Johannes Eichler <jo.eichler@gmx.de>
2015-04-05 00:22:25 +0200

    tried to hidde navigation bar

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -10,16 +10,70 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var memeEditorImage: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // The status bar should be hidden while in meme edit view to use the maximum available screen size for the image.
+        
+        self.hideStatusBar()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(true)
+
+        self.subscribeToOrientationChangeNotification()
+    
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func subscribeToOrientationChangeNotification() {
+        
+        // To check if the statusBar is visible afte orientation change
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "hideStatusBar", name: UIDeviceOrientationDidChangeNotification, object: nil)
+    
+    }
+    
+    func hideStatusBar(){
+        // Checks if the status bar is visible and adjusts the memeEditorImage height accordingly. 
+        
+        if (!UIApplication.sharedApplication().statusBarHidden){
+            
+            UIApplication.sharedApplication().setStatusBarHidden(true, withAnimation: UIStatusBarAnimation.Slide)
+
+        }
+        else {
+            
+            println("The Status Bar is visible")
+        
+        
+        }
+        
+    
+    
+    }
 
 
+
+    @IBAction func photoFromCamera(sender: UIBarButtonItem) {
+    }
+    
+    
+    @IBAction func photoFromAlbum(sender: UIBarButtonItem) {
+    }
+    
+    
+    @IBAction func openActivityCenter(sender: UIBarButtonItem) {
+    }
+
+    @IBAction func cancelMemeEditor(sender: UIBarButtonItem) {
+    }
 }
 
